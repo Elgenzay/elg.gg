035bd4c256a2e4fe1fd45b2144507df8824d6fd3
Jason <jason@elg.gg>
2023-12-12 17:36:06 -0500

    Add version route & redirect fix

--- https-redirect/src/main.rs
+++ https-redirect/src/main.rs
@@ -1,17 +1,34 @@
-use rocket::response::Redirect;
+use rocket::{
+	response::Redirect,
+	serde::{json::Json, Serialize},
+};
 use std::path::PathBuf;
 
 #[rocket::get("/<path..>")]
 pub async fn redirect(path: PathBuf) -> Redirect {
-	let mut new_uri = String::from("https://");
 	let domain = std::env::var("DOMAIN").expect("Missing environment variable: DOMAIN");
-	new_uri.push_str(&domain);
-	new_uri.push_str(&path.into_os_string().into_string().unwrap());
+	let new_uri = format!(
+		"https://{}/{}",
+		domain,
+		path.into_os_string().into_string().unwrap()
+	);
 	Redirect::to(new_uri)
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
 	dotenvy::dotenv().ok();
-	rocket::build().mount("/", rocket::routes![redirect])
+	rocket::build().mount("/", rocket::routes![redirect, version])
+}
+
+#[derive(Serialize)]
+pub struct VersionInfo {
+	version: String,
+}
+
+#[rocket::get("/version")]
+pub fn version() -> Json<VersionInfo> {
+	Json(VersionInfo {
+		version: env!("CARGO_PKG_VERSION").to_string(),
+	})
 }
